1e110d6e6ef39f9f09d8ebabf1414504ba4b183a
谷祖楠 <13793165851@163.com>
2017-06-13 14:45:18 +0800

    添加入口类

---
+++ src/Controllers/CategoryController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2017, iBenchu.org
+ * @datetime 2017-02-23 19:44
+ */
+namespace Notadd\Slide\Controllers;
+
+use Notadd\Foundation\Routing\Abstracts\Controller;
+use Notadd\Slide\Models\Category;
+
+/**
+ * Class BaiduController.
+ */
+class CategoryController extends Controller
+{
+    public function create()
+    {
+
+    }
+}
