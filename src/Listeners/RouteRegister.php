8902a27365a2b119ea43734654d6385ea6e89ffe
谷祖楠 <13793165851@163.com>
2017-06-13 14:45:52 +0800

    注册路由

--- src/Listeners/RouteRegister.php
+++ src/Listeners/RouteRegister.php
@@ -7,6 +7,7 @@
  */
 namespace Notadd\Slide\Listeners;
 
+use Notadd\Slide\Controllers\CategoryController;
 use Notadd\Slide\Controllers\SlideController;
 use Notadd\Foundation\Routing\Abstracts\RouteRegister as AbstractRouteRegister;
 
@@ -20,9 +21,14 @@ class RouteRegister extends AbstractRouteRegister
      */
     public function handle()
     {
-        $this->router->group(['middleware' => ['auth:api', 'cross', 'web'], 'prefix' => 'api/baidu'], function () {
-            $this->router->post('get', BaiduController::class . '@get');
-            $this->router->post('set', BaiduController::class . '@set');
+        $this->router->group(['middleware' => ['cross', 'web'], 'prefix' => 'api/slide'], function () {
+            $this->router->get('index', SlideController::class . '@index');
         });
+
+        $this->router->group(['middleware' => ['cross', 'web'], 'prefix' => 'api/slide/category'],function() {
+            $this->router->get('edit', CategoryController::class.'@getEdit');
+            $this->router->post('edit', CategoryController::class.'@postEdit');
+            $this->router->post('delete', CategoryController::class.'@postDelete');
+        })
     }
 }
