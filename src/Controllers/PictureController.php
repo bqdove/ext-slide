e69c6372eed63ac57230238bb66ff05a6e460f8c
谷祖楠 <13793165851@163.com>
2017-06-14 10:50:06 +0800

    修改PictureController

--- src/Controllers/PictureController.php
+++ src/Controllers/PictureController.php
@@ -7,26 +7,31 @@
  */
 namespace Notadd\Slide\Controllers;
 
-use Notadd\Foundation\Routing\Abstracts\Controller;
-use Notadd\Slide\Models\Picture;
+use Notadd\Slide\Handlers\DeleteHandler;
+use Notadd\Slide\Handlers\GetHandler;
+use Notadd\Slide\Handlers\UploadHandler;
 
-/**
- * Class CategoryController.
- */
-class PictureController extends Controller
+class PictureController
 {
-    public function getEdit()
+    public function upload(UploadHandler $handler)
     {
-
+        header("Access-Control-Allow-Origin: *");
+        return $handler->toResponse()->generateHttpResponse();
     }
 
-    public function postEdit()
+    public function lists(GetHandler $handler)
     {
-
+        return $handler->toResponse()->generateHttpResponse();
     }
 
-    public function postDelete()
-    {
+    public function delete(deleteHandler $deleteHander){
+        return $deleteHander->toResponse()->generateHttpResponse();
+    }
 
+    private function mk_dir($dir, $mode = 0755)
+    {
+        if (is_dir($dir) || @mkdir($dir,$mode)) return true;
+        if (!$this->mk_dir(dirname($dir),$mode)) return false;
+        return @mkdir($dir,$mode);
     }
 }
