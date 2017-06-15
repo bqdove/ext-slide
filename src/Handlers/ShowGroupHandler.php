dfe9e5a3197de5e8f11310eed9ded656e8c3f75d
谷祖楠 <13793165851@163.com>
2017-06-15 21:41:35 +0800

    添加图集现实状态的功能实现

--- src/Handlers/ShowGroupHandler.php
+++ src/Handlers/ShowGroupHandler.php
@@ -15,6 +15,28 @@ class ShowGroupHandler extends AbstractSetHandler
 {
     public function execute()
     {
+        $groupId = $this->request->input('group_id');
 
+        $group = Group::where('alias', $groupId)->get();
+
+        if ($group->show)
+        {
+            $status = 0;
+        }else{
+            $status = 1;
+        }
+
+        $group->show = $status;
+
+        $result = $group->save();
+
+        if ($result)
+        {
+            $this->success()->withData([
+                'show'  => object_get($group, 'show')
+            ])->withMessage('获取显示状态数据成功！');
+        }else{
+            $this->withCode('401')->withMessage('获取现实状态失败');
+        }
     }
 }
