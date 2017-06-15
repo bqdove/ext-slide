87088a894624cf9442f1695c29a995c771464e04
谷祖楠 <13793165851@163.com>
2017-06-15 17:23:30 +0800

    完成删除分类功能

--- src/Handlers/DeleteCategoryHandler.php
+++ src/Handlers/DeleteCategoryHandler.php
@@ -11,6 +11,8 @@ namespace Notadd\Slide\Handlers;
 use Illuminate\Support\Facades\Storage;
 use Notadd\Foundation\Passport\Abstracts\SetHandler as AbstractSetHandler;
 use Notadd\Slide\Models\Category;
+use Notadd\Slide\Models\Group;
+use Notadd\Slide\Models\Picture;
 
 /**
  * Class ConfigurationHandler.
@@ -24,40 +26,46 @@ class DeleteCategoryHandler extends AbstractSetHandler
      */
     public function execute()
     {
-        $cateId = $this->request->input('category_id');
+        $cateId = $this->request->get('category_id');
 
         $category = Category::where('alias', $cateId)->first();
 
         if (!$category)
         {
-            return false;
+            return $this->withCode('404')->withMessage('请重新确认分类Id是否正确');
         }
-        Category::beginTransaction();
-        try{
 
-            Category->where('alias',$cateId)->delete();
-        }catch (\PDOException $PDOException){
-            DB::rollback();
-            $this->messages->push($this->translator->trans('ImagesManager::delete.fail'));
-            return false;
-        }
-        try{
-            if($mFiles)
-                foreach ($mFiles->toArray() as $v){//如果有，则删除文件
-                    if(Storage::disk('public')->exists($v->path)){
-                        Storage::disk('public')->delete($v->path);
-                    }
+        $groups = $category->groups()->with('pictures')->get();
 
+        foreach($groups as $group)
+        {
+            $pictures = $group->pictures()->get();
+            //循环删除某个图集的所有图片
+            foreach($pictures as $picture)
+            {
+                try{
+                    $picture->delete();
+                }catch (\PDOException $PDOException){
+                    Picture::rollback();
+                    return false;
                 }
-        }catch (\Exception $exception){
-            DB::rollback();
-            $this->messages->push($this->translator->trans('ImagesManager::delete.fail'));
-            return false;
+            }
+            //图集图片循环删除完毕后尝试删除当前图集信息
+            try{
+                $group->delete();
+            }catch (\PDOException $PDOException){
+                Group::rollback();
+                return false;
+            }
         }
 
-        DB::commit();
-        $this->messages->push($this->translator->trans('ImagesManager::delete.success'));
-        return true;
-
+        //如果当前分类的搜有图集及其所含图片删除完毕后，尝试删除当前想要删除的分类
+        try{
+            $category->delete();
+            $this->success()->withMessage('删除分类成功');
+        }catch (\PDOException $PDOException){
+            Category::rollback();
+            return false;
+        }
     }
 }
