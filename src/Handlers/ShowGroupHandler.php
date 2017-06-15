7f7f68b64986abfd9ee18daafed531e167526a44
谷祖楠 <13793165851@163.com>
2017-06-15 21:21:01 +0800

    添加显示图集接口

---
+++ src/Handlers/ShowGroupHandler.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @copyright (c) 2017, iBenchu.org
+ * @datetime 2017-06-14 19:45
+ */
+namespace Notadd\Slide\Handlers;
+
+use Notadd\Foundation\Passport\Abstracts\SetHandler as AbstractSetHandler;
+use Notadd\Slide\Models\Group;
+
+
+class ShowGroupHandler extends AbstractSetHandler
+{
+    public function execute()
+    {
+
+    }
+}
