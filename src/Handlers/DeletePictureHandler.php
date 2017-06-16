d21d4508d12a37c2a3c7453428a57bc59eb79eba
谷祖楠 <13793165851@163.com>
2017-06-16 23:17:26 +0800

    完成删除图片功能

--- src/Handlers/DeletePictureHandler.php
+++ src/Handlers/DeletePictureHandler.php
@@ -1,15 +1,48 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: bc021
- * Date: 17-6-16
- * Time: 下午6:27
+ * This file is part of Notadd.
+ *
+ * @author Allen <674397601@qq.com>
+ * @copyright (c) 2017, iLeyun.org
+ * @datetime 2017-06-14 19:45
  */
-
 namespace Notadd\Slide\Handlers;
 
+use Notadd\Foundation\Passport\Abstracts\SetHandler as AbstractSetHandler;
+use Notadd\Slide\Models\Picture;
 
-class DeletePictureHandler
+/**
+ * Class ConfigurationHandler.
+ */
+class DeletePictureHandler extends AbstractSetHandler
 {
+    /**
+     * Execute Handler.
+     *
+     * @return bool
+     */
+    public function execute()
+    {
+        $path = $this->request->query('path');
 
+        $picture = Picture::where('path', $path)->first();
+
+        if ($picture)
+        {
+            $picture->delete();
+        }
+
+        if ($this->container->make('files')->exists(base_path($path)))
+        {
+            $this->container->make('files')->delete(base_path($path));
+        }
+
+        if (Picture::where('path', $path)->first() || $this->container->make('files')->exists(base_path($path)))
+        {
+            return $this->withCode('402')->withMessage('删除失败，请稍候再试');
+        }
+
+        return $this->success()->withMessage('删除成功');
+
+    }
 }
