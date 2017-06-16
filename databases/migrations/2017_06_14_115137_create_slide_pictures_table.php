02871771d369ea445d91bd97a73f0ea2caf99267
谷祖楠 <13793165851@163.com>
2017-06-16 23:05:14 +0800

    修改数据库错误字段

--- databases/migrations/2017_06_14_115137_create_slide_pictures_table.php
+++ databases/migrations/2017_06_14_115137_create_slide_pictures_table.php
@@ -24,10 +24,10 @@ class CreateSlidePicturesTable extends Migration
             $this->schema->create('slide_pictures', function (Blueprint $table) {
                 $table->increments('id')->comment('图片ID');//图片ID
                 $table->integer('user_id')->comment('图片上传用户id');//图片上传用户id
-                $table->string('path')->comment('图片路径');//图片路径
-                $table->string('hash')->comment('图片hash，防重');//图片加密路径
+                $table->string('name')->comment('图片名称');//图片路径
+                $table->string('path')->comment('图片路径，图片名MD5');//图片加密路径
                 $table->string('title',64)->nullable()->comment('图片标题');
-                $table->string('backgroud', 10)->comment('填充颜色');
+                $table->string('background', 10)->comment('填充颜色');
                 $table->unsignedInteger('group_id')->comment('图片所属组Id');
                 $table->softDeletes();
                 $table->timestamps();
