02b1149c6b8ea978a01a9b9a302fdc867b86fd2b
谷祖楠 <13793165851@163.com>
2017-06-16 22:40:34 +0800

    修改获取方法

--- src/Handlers/GetPictureHandler.php
+++ src/Handlers/GetPictureHandler.php
@@ -1,15 +1,39 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: bc021
- * Date: 17-6-16
- * Time: 下午6:27
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2017, iBenchu.org
+ * @datetime 2017-04-10 19:41
  */
-
 namespace Notadd\Slide\Handlers;
 
+use Notadd\Foundation\Routing\Abstracts\Handler;
+use Notadd\Slide\Models\Group;
 
-class GetPictureHandler
+/**
+ * Class GetHandler.
+ */
+class GetPictureHandler extends Handler
 {
+    /**
+     * Execute Handler.
+     *
+     * @throws \Exception
+     */
+    protected function execute()
+    {
+        $groupId = $this->request->get('group_id',null);
 
+        if ($groupId){
+            $group = Group::where('alias', $groupId)->first();
+
+            $pictures = $group->pictures()->get();
+
+            return $this->success()->withData($pictures)->withMessage('获取图集详情数据成功！');
+        }else{
+            return $this->withCode('402')->withMessage('获取图片详情失败，请稍后重试');
+        }
+
+    }
 }
