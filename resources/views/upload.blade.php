47bcdeb620c2875eb852e5e9739af412a93e7477
谷祖楠 <13793165851@163.com>
2017-06-20 21:06:40 +0800

    调试测试页面

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -5,7 +5,7 @@
 <body>
 <h1>文件上传</h1>
 
-<form action="{{url('/api/slide/picture/upload')}}" method="post" enctype="multipart/form-data">
+<form action="{{url('/api/slide/picture/upload?group_id=388')}}" method="post" enctype="multipart/form-data">
     {{csrf_field()}}
     <input type="file" name="file" >
     <input type="submit" name="dosubmit" value="上传">
