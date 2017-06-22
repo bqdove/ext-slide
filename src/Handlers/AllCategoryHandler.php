77802398f2647a74856a6506581c3c97aa153904
谷祖楠 <13793165851@163.com>
2017-06-22 18:26:48 +0800

    将分类列表接口分离

---
+++ src/Handlers/AllCategoryHandler.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @copyright (c) 2017, iBenchu.org
+ * @datetime 2017-04-10 19:41
+ */
+namespace Notadd\Slide\Handlers;
+
+use Notadd\Foundation\Routing\Abstracts\Handler;
+use Notadd\Slide\Models\Category;
+
+
+/**
+ * Class GetHandler.
+ */
+class AllCategoryHandler extends Handler
+{
+    /**
+     * Execute Handler.
+     *
+     * @throws \Exception
+     */
+    protected function execute()
+    {
+        $categories = Category::all();
+
+        $this->success()->withData($categories)->withMessage('获取数据成功！');
+    }
+}
