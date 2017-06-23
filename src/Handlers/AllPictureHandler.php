7c6e79d65887b33572fe04c039dc9f6a855f1205
谷祖楠 <13793165851@163.com>
2017-06-23 15:38:34 +0800

    添加图片列表接口

---
+++ src/Handlers/AllPictureHandler.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @copyright (c) 2017, iBenchu.org
+ * @datetime 2017-04-10 19:41
+ */
+namespace Notadd\Slide\Handlers;
+
+use Notadd\Foundation\Routing\Abstracts\Handler;
+use Notadd\Slide\Models\Picture;
+
+
+/**
+ * Class GetHandler.
+ */
+class ALlPictureHandler extends Handler
+{
+    /**
+     * Execute Handler.
+     *
+     * @throws \Exception
+     */
+    protected function execute()
+    {
+        $this->validate($this->request, [
+            'group_id' => 'required'
+        ],[
+            'group_ip.required' => '图集id为必填字段'
+        ]);
+
+        $pictures = Picture::where('group_id', $this->request->input('group_id'))->get()->toArray();
+        if (count($pictures) > 0)
+        {
+            $this->success()->withData($pictures)->withMessage('获取数据成功！');
+        }else{
+            $this->withCode(402)->withError('此图集为空');
+        }
+    }
+}
