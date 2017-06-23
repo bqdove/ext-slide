896fcc29916ca01acb33175a2d9d6d043eb58195
谷祖楠 <13793165851@163.com>
2017-06-23 15:41:03 +0800

    添加Category的fillable

--- src/Models/Category.php
+++ src/Models/Category.php
@@ -8,6 +8,8 @@ class Category extends Model
 {
 	protected $table = 'slide_categories';
 
+	protected $fillable = ['category_name', 'alias'];
+
 	public function groups()
     {
         return $this->hasMany('Notadd\Slide\Models\Group');
