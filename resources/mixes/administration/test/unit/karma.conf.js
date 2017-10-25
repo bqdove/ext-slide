09ced4bf42e3e9ea3e47638fd1f774c199319c98
tianjign <tfanxi@163.com>
2017-10-25 15:36:43 +0800

    add karma configuration

--- resources/mixes/administration/test/unit/karma.conf.js
+++ resources/mixes/administration/test/unit/karma.conf.js
@@ -1,55 +1,26 @@
 // Karma configuration
 
-const webpackConfig = require('../../build/webpack.test.conf');
+var webpackConfig = require('../../build/webpack.test.conf');
 
-module.exports = config => {
+module.exports = function(config) {
     config.set({
-        // browsers: ['PhantomJS'],
-        browsers: ['Chrome'],
-
+        browsers: ['PhantomJS'],
         frameworks: ['mocha', 'sinon-chai'],
-
-        files: ['./file.js'],
-
+        reporters: ['spec', 'coverage'],
+        files: ['./index.js'],
         preprocessors: {
-            './file.js': ['webpack', 'sourcemap', 'coverage']
+            './index.js': ['webpack', 'sourcemap']
         },
-
         webpack: webpackConfig,
-
-        plugins: [
-            'karma-webpack',
-            'karma-sourcemap-loader',
-            'karma-mocha',
-            'karma-chai',
-            'karma-sinon-chai',
-            'karma-chrome-launcher',
-            // 'karma-phantomjs-launcher',
-            'karma-spec-reporter',
-            'karma-coverage'
-        ],
-
-        reporters: ['spec', 'coverage', 'progress'],
-
+        webpackMiddleware: {
+            noInfo: true
+        },
         coverageReporter: {
             dir: './coverage',
             reporters: [
-                { type: 'html', subdir: 'report-html' },
                 { type: 'lcov', subdir: '.' },
                 { type: 'text-summary' }
             ]
-        },
-
-        port: 9876,
-
-        colors: true,
-
-        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-        logLevel: config.LOG_INFO,
-
-        // enable / disable watching file and executing tests whenever any file changes
-        autoWatch: false,
-
-        concurrency: Infinity
-    })
-}
+        }
+    });
+};
