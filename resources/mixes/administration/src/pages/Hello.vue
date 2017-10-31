105856bca39ca560927a2363dd7188ac66072c8e
tianjign <tfanxi@163.com>
2017-10-31 16:56:59 +0800

    modify karma config

--- resources/mixes/administration/src/pages/Hello.vue
+++ resources/mixes/administration/src/pages/Hello.vue
@@ -1,5 +1,7 @@
 <template>
-    <div>msg</div>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+    </div>
 </template>
 <script>
     export default {
--- resources/mixes/administration/test/unit/specs/test.spec.js
+++ resources/mixes/administration/test/unit/specs/test.spec.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
-import Test from '../../../src/pages/test.vue'
+import Hello from '../../../src/pages/Hello.vue'
 
-describe('test.vue', () => {
+describe('Hello.vue', () => {
     it('should render correct contents', () => {
-        const Constructor = Vue.extend(Test)
+        const Constructor = Vue.extend(Hello)
         const vm = new Constructor().$mount()
         expect(vm.$el.querySelector('.hello h1').textContent)
             .to.equal('Welcome to Your Vue.js App')
